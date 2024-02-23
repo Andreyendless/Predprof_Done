e2b9826c53803cdc5b099bf63027e7cfcaf563af
Andreyendless <reliable1055@gmail.com>
2024-02-23 13:08:49 +0300

    task_3 binary_search not finished

--- prog_z3_poisk.py
+++ prog_z3_poisk.py
@@ -1,4 +1,38 @@
 import csv
+##=============================================================================
+## Линейный поиск
+'''
+with open('students.csv', encoding='utf-8') as file:
+    reader = list(csv.DictReader(file, delimiter=',', quotechar='"'))
+    id_input = input()
+    while id_input != 'СТОП':
+        id_input = int(id_input)
+        fl = 0
+        for reader[id_found] in reader:
+            if int(reader[id_found]['titleProject_id']) == id_input:
+                fl = 1
+                student_all = reader[id_found]["Name"].split()
+                student_cut = student_all[1][0] + '. ' + student_all[0]
+                print(f'Проект № {reader[id_found]["titleProject_id"]} делал: {\
+                    student_cut} он(а) получил(а) оценку - {reader[id_found]["score"]}')
+                break
+        if fl == 0:
+            print('Ничего не найдено')
+        id_input = input()
+'''
+##=============================================================================
+## Бинарный поиск
+def binary_search(arr, value):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        middle = (left+right)//2
+        if value == int(arr[middle]['titleProject_id']):
+            return middle
+        elif value > int(arr[middle]['titleProject_id']):
+            left = middle + 1
+        else:
+            right = middle - 1
+    return -1
 
 with open('students.csv', encoding='utf-8') as file:
     reader = list(csv.DictReader(file, delimiter=',', quotechar='"'))
@@ -6,23 +40,18 @@ with open('students.csv', encoding='utf-8') as file:
         cursor = reader[i]
         cursor_score = int(cursor['score'] if cursor['score']!='None' else 0)
         pos = i
-        while pos > 0 and int(reader[pos-1]['score'] if reader[pos-1]['score']!='None' else 0)\
-                < cursor_score:
+        while pos > 0 and int(reader[pos-1]['score'] if\
+            reader[pos-1]['score']!='None' else 0) < cursor_score:
             reader[pos] = reader[pos-1]
             pos -= 1
         reader[pos] = cursor
     id_input = input()
     while id_input != 'СТОП':
-        id_input = int(id_input)
-        fl = 0
-        for student in reader:
-            if int(student['titleProject_id']) == id_input:
-                fl = 1
-                student_all = student["Name"].split()
-                student_cut = student_all[1][0] + '. ' + student_all[0]
-                print(f'Проект № {student["titleProject_id"]} делал: {student_cut} он(а) получил(а) оценку - {student["score"]}')
-                break
-        if fl == 0:
+        id_found = binary_search(reader, int(id_input))
+        if id_found == -1:
             print('Ничего не найдено')
+        else:
+            student_all = reader[id_found]["Name"].split()
+            student_cut = student_all[1][0] + '. ' + student_all[0]
+            print(f'Проект № {reader[id_found]["titleProject_id"]} делал: {student_cut} он(а) получил(а) оценку - {reader[id_found]["score"]}')
         id_input = input()
-    
